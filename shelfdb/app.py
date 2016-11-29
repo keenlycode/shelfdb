06bec8310f6c7b0146a13f50b2acb9d89ecec363
Um Nontasuwan <nitipit@gmail.com>
2016-11-29 14:58:01 +0700

    update

--- shelfdb/app.py
+++ shelfdb/app.py
@@ -1,5 +1,5 @@
 import asyncio, shelve, os, collections
-from uuid import uuid1
+from uuid import uuid4
 
 class DB:
     def __init__(self, *args, **kw):
@@ -13,10 +13,12 @@ class Shelf(dict):
     def __init__(self, db_dir, *args, **kw):
         self._db_dir = db_dir
 
-    def __getitem__(self, k, *args, **kw):
-        if not k in self:
+    def __getitem__(self, k):
+        if (not k in self or
+                isinstance(super().__getitem__(k).dict, shelve._ClosedDict)):
             super().__setitem__(
                 k, FileShelf(os.path.join(self._db_dir, k)))
+
         return super().__getitem__(k)
 
     def get(self, k, *args, **kw):
@@ -28,17 +30,22 @@ class FileShelf(shelve.DbfilenameShelf):
         self._filename = filename
         return super().__init__(filename, *args, **kw)
 
-    def __iter__(self, *args, **kw):
-        for item in super().__iter__(*args, **kw):
+    def __setitem__(self, id_, obj):
+        if not isinstance(obj, dict):
+            raise ValueError('Value must be `dict` instance')
+        super().__setitem__(id_, obj)
+
+    def all(self, *args, **kw):
+        for item in self:
             result = self[item]
             result.update({'_id': item})
             yield result
 
-    def insert(self, obj=None, id_=str(uuid1())):
-        return super().__setitem__(id_, obj)
-
-    def items(self, *args, **kw):
-        return self.__iter__(*args, **kw)
+    def insert(self, obj=None, id_=None):
+        # Since id_=str(uuid4()) in def args will return the same value
+        if id_ is None:
+            id_ = str(uuid4())
+        self[id_] = obj
 
     def get(self, k, *args, **kw):
         result = super().__getitem__(k)
@@ -46,14 +53,16 @@ class FileShelf(shelve.DbfilenameShelf):
         return result
 
     def filter(self, fn):
-        try:
-            for result in filter(fn, self):
-                yield result
-        except:
-            pass
+        result = filter(fn, self.all())
+        while True:
+            try:
+                yield next(result)
+            except KeyError:
+                pass
 
     def delete(self, *args, **kw):
-        self.clear()
+        for k in self.keys():
+            del self[k]
         self.close()
         os.remove(self._filename)
 

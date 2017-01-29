2117a0f8dac616de22ddcb2bfa52d867ff454550
Um Nontasuwan <nitipit@gmail.com>
2017-01-30 04:35:31 +0700

    bump versoin

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shelfdb',
-    version='0.1.2',
+    version='0.1.3',
     description='Python dict/json DB, Done right for Efficiency and Simplicity',
     url='https://github.com/nitipit/shelfdb',
     author='Nitipit Nontasuwan',

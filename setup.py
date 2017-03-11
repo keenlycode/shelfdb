8f90d90320edd36d251be22248aa35a3fd2ce193
Um Nontasuwan <nitipit@gmail.com>
2017-03-11 13:03:54 +0700

    version to 0.2.0-rc3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shelfdb',
-    version='0.2.0-rc2',
+    version='0.2.0-rc3',
     description='Python dict/json DB, Done right for Efficiency and Simplicity',
     url='https://github.com/nitipit/shelfdb',
     author='Nitipit Nontasuwan',

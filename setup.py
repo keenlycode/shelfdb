e05e6c9dfb20978ece3638bf6292dd449fa568b7
Um Nontasuwan <nitipit@gmail.com>
2017-03-23 22:46:17 +0700

    bump to 0.2.2-dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shelfdb',
-    version='0.2.1',
+    version='0.2.2-dev',
     description='Python dict/json DB, Done right for Efficiency and Simplicity',
     long_description='Python dict/json DB, Done right for Efficiency and Simplicity',
     url='https://github.com/nitipit/shelfdb',

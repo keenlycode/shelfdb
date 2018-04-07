b3eec7b6d6ad44ee56ed18440ab2c19efdd3a7ad
Nitipit Nontasuwan <nitipit@gmail.com>
2018-04-07 23:33:00 +0700

    bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shelfdb',
-    version='0.2.2-dev',
+    version='0.2.2',
     description='Python dict/json DB, Done right for Efficiency and Simplicity',
     long_description='Python dict/json DB `done right` to make your job done',
     url='https://github.com/nitipit/shelfdb',
@@ -11,7 +11,7 @@ setup(
     author_email='nitipit@gmail.com',
     license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='dict json database',
     packages=find_packages(),

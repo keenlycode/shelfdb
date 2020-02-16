8b7f40772665d9300407f851b6cddeea1e2e9123
= <nitipit@gmail.com>
2020-02-16 21:47:54 +0700

    update test

--- shelfdb/test_server.py
+++ shelfdb/test_server.py
@@ -20,10 +20,11 @@ class Field(BaseField):
 class Note(Model):
     _id = Field().uuid1()
     title = Field().required().type(str)
+    note = Field().type(str)
     datetime = Field().default(datetime.utcnow)
 
 
-class TestShelfServer(unittest.TestCase):
+class TestRetrieveData(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.shelfdb_process = Process(target=server.main, daemon=True)
@@ -60,10 +61,52 @@ class TestShelfServer(unittest.TestCase):
         note = self.db.shelf('note').get(self.test_notes[0]['_id']).run()
         Note(note)
 
-    # def test_entry(self):
-    #     self.db.shelf('note').first().entry(lambda entry: entry.update({'note': 'test_entry'})).run()
-    #     note = self.db.shelf('note').first().run()
-    #     print(note)
+    def test_first(self):
+        note = self.db.shelf('note').first().run()
+        note = Note(note)
+
+    def test_filter(self):
+        notes = self.db.shelf('note').filter(lambda note: note['title'] == 'note-1').run()
+        self.assertIsInstance(notes, list)
+        self.assertEqual(len(notes), 1)
+        self.assertEqual(notes[0]['title'], 'note-1')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.shelfdb_process.terminate()
+        shutil.rmtree(Path('db'))
+
+
+class TestModifyData(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.shelfdb_process = Process(target=server.main, daemon=True)
+        cls.shelfdb_process.start()
+        cls.db = shelfquery.db()
+        cls.test_notes = []
+        test_notes = [
+            dict(Note({'title': 'note-1'})),
+            dict(Note({'title': 'note-2'})),
+            dict(Note({'title': 'note-3'})),
+        ]
+        i = 0
+        while True:
+            sleep(0.1)
+            try:
+                cls.db.shelf('note').entry(lambda entry: entry).run()
+                break
+            except ConnectionRefusedError:
+                if i >= 10:
+                    raise TimeoutError
+                i += 1
+        for note in test_notes:
+            _id = cls.db.shelf('note').insert(note).run()
+            note['_id'] = _id
+            cls.test_notes.append(note)
+
+    def test_entry(self):
+        self.db.shelf('note').first().entry(lambda entry: entry.update({'title': 'entry'})).run()
+        self.assertEqual(self.db.shelf('note').first().run()['title'], 'entry')
 
     @classmethod
     def tearDownClass(cls):

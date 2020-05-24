02b3728ab323e1ac0885bdf752827593c247fb7a
Nitipit Nontasuwan <nitipit@gmail.com>
2020-05-24 17:20:30 +0700

    v0.6.2 - docs

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+import sys
 
+
+install_requires = ['dill>=0.3.0']
+if sys.platform.startswith('linux'):
+    install_requires.append('uvloop=>0.12.1')
 
 setup(
     name='shelfdb',
-    version='0.6.2dev',
+    version='0.6.2',
     description='Python dictionary database with asyncio server',
     long_description='Python dictionary database with asyncio server',
     url='https://github.com/nitipit/shelfdb',
@@ -14,7 +19,8 @@ setup(
     python_requires='>=3.6',
     keywords='dict json database',
     packages=find_packages(),
-    install_requires=['uvloop>=0.12.1', 'dill>=0.2.9'],
+    install_requires=install_requires,
     tests_require=['shelfquery', 'dictify'],
     entry_points={'console_scripts': ['shelfdb=shelfdb.server:main']},
 )
+
